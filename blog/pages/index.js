3972317ec682a364c0faac9541f746767f9a976d
itc-s23009 <s23009@std.it-college.ac.jp>
2024-05-21 03:01:36 +0000

    Hello world に変更

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,11 +1,8 @@
-// Import with a different name
-import HeroComponent from './components/hero'
-
-// Use a different name for the constant
 const CustomHero = () => {
   return (
-    <HeroComponent title='Hello World!!' subtitle='新しい世界へ' imageOn />
-  )
+    <h1>Hello World</h1>
+  );
 }
 
-export default CustomHero
+export default CustomHero;
+
